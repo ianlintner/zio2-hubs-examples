ff8e3105d585d7472858225dbb3ef24f79a5e4d2
Ian Lintner <lintner.ian@gmail.com>
2023-03-07 14:41:07 -0600

    Util function comments.

--- src/main/scala/ShameUtil.scala
+++ src/main/scala/ShameUtil.scala
@@ -7,6 +7,12 @@ import java.io.IOException
  */
 object ShameUtil {
 
+  /** Wrapper function to generate variable count of zio consumers
+   * @param hub hub to consume
+   * @param consumerCount number of consumers to spin up.
+   * @param delay int delay in seconds
+   * @return ZIO Unit
+   */
   def hubConsumers(hub: Hub[Take[Nothing, String]], consumerCount: Int, delay: Int = 1): URIO[Scope, Unit] =
     ZIO
       .foreachParDiscard(1 to consumerCount) { i =>
@@ -14,6 +20,12 @@ object ShameUtil {
       }
       .orDie
 
+  /**
+   * Gets ZStream Based Hub Consumer
+   * @param id string id for logging.
+   * @param hub the hub to consume.
+   * @return ZIO Long
+   */
   def getStreamingConsumer(id: String, hub: Hub[Take[Nothing, String]]): ZIO[Any, IOException, Long] = ZStream
     .fromHub(hub)
     .flattenTake
@@ -22,6 +34,12 @@ object ShameUtil {
       .contramapChunks[String](_.flatMap(x => s"> $id $x \n".getBytes))
     )
 
+  /** Get a ZIO based consumer for hub.
+   * @param id string id for logging.
+   * @param hub the hub to consume.
+   * @param delay int delay in seconds.
+   * @return ZIO Long
+   */
   def getConsumer(id: String, hub: Hub[Take[Nothing, String]], delay: Int = 1): ZIO[Scope, IOException, Long] = for {
     subscription <- hub.subscribe
     subscriber <- subscription.poll.flatMap {
@@ -30,6 +48,11 @@ object ShameUtil {
     }.schedule(Schedule.spaced(delay.seconds))
   } yield subscriber
 
+  /** Log size of a hub.
+   * @param hub generic hub
+   * @tparam A hub type
+   * @return ZIO Unit
+   */
   def sized[A](hub: Hub[A]): ZIO[Any, Nothing, Unit] =
     hub.size.flatMap(i => ZIO.logInfo(s"Hub Size: $i"))
 
